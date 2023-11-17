5b78338fabb115c8c4e18c21c4572088c5e1cc29
Minsu Kim <mindf9908@gmail.com>
2023-11-17 18:28:39 +0900

    fisrt commit

---
+++ src/main/java/test/test/TestApplication.java
@@ -0,0 +1,29 @@
+package test.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@SpringBootApplication
+public class TestApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@GetMapping("/ip")
+	public Map<String, String> ip(HttpServletRequest request) throws UnknownHostException {
+		return new HashMap<>(){{
+			put("hostIp", InetAddress.getLocalHost().getHostAddress());
+			put("accessIp", request.getHeader("x-forwarded-for"));
+		}};
+	}
+}
+
